f3b8602d7a4ca8954535be050c6c7ca12287f750
sengawasenga <14marcel.senga@gmail.com>
2023-02-21 02:36:42 -0800

    Add a program to print out base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all single digit numbers of base 10
+ * starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
